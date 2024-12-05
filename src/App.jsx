b4ae0db056dc36f4566949efab98bf05fc8f87c9
Skiddler233 <joelseaman96@gmail.com>
2024-12-05 15:36:43 +1100

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import WelcomeScreen from "./components/WelcomeScreen";
 import TermScreen from "./components/TermScreen";
 import AboutYouPage from "./components/AboutYouPage";
@@ -10,17 +10,15 @@ import ResultsPage from "./components/ResultsPage";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<WelcomeScreen />} />
-        <Route path="/terms" element={<TermScreen />} />
-        <Route path="/about" element={<AboutYouPage />} />
-        <Route path="/quiz" element={<QuizPage />} />
-        <Route path="/quiz2" element={<QuizPage2 />} />
-        <Route path="/calculating" element={<CalculatingResults />} />
-        <Route path="/results" element={<ResultsPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<WelcomeScreen />} />
+      <Route path="/terms" element={<TermScreen />} />
+      <Route path="/about" element={<AboutYouPage />} />
+      <Route path="/quiz" element={<QuizPage />} />
+      <Route path="/quiz2" element={<QuizPage2 />} />
+      <Route path="/calculating" element={<CalculatingResults />} />
+      <Route path="/results" element={<ResultsPage />} />
+    </Routes>
   );
 };
 
--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./main.css";
+import './main.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/MandyMoney">
       <App />
     </BrowserRouter>
   </React.StrictMode>
