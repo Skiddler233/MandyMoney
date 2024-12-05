4b5fe92183562ff1b1369e0505cd046af6beb535
Skiddler233 <joelseaman96@gmail.com>
2024-12-05 15:27:33 +1100

    testing

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
 import App from "./App";
 import './main.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Add BrowserRouter with the basename */}
+    <BrowserRouter basename="/MandyMoney">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
